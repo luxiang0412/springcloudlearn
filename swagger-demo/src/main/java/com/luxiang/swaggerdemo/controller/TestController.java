fd59d15cea20ba7b521dbbedd5cf930d52bed8ae
luxiang <1374920889@qq.com>
2020-02-21 19:28:46 +0800

    修改

---
+++ swagger-demo/src/main/java/com/luxiang/swaggerdemo/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.luxiang.swaggerdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+    
+}
---
+++ swagger-demo/src/test/java/com/luxiang/swaggerdemo/SwaggerDemoApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.luxiang.swaggerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
