c6e6c34a36aa7457b26285c10d9002bce537af34
luxiang <1374920889@qq.com>
2020-04-22 14:50:58 +0800

    修改

---
+++ mongo/src/main/java/com/luxiang/mongo/repository/ArticleRepository.java
@@ -0,0 +1,17 @@
+package com.luxiang.mongo.repository;
+
+import com.luxiang.mongo.model.Article;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+
+public interface ArticleRepository extends MongoRepository<Article, Long> {
+    /**
+     * 根据标题模糊查询
+     *
+     * @param title 标题
+     * @return 满足条件的文章列表
+     */
+    List<Article> findByTitleLike(String title);
+}
