848e04a69310ea6cf6530c122583ee4a0cb5b20c
luxiang <1374920889@qq.com>
2021-01-15 06:23:12 +0800

    okhttp

---
+++ okhttp-demo/src/main/java/com/luxiang/okhttpdemo/OkhttpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.luxiang.okhttpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkhttpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkhttpDemoApplication.class, args);
+	}
+
+}
---
+++ okhttp-demo/src/test/java/com/luxiang/okhttpdemo/OkhttpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxiang.okhttpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkhttpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
